ee23f9467bcd74c1051ee1001b087b84cf41c097
HenryLooo <94285262+HenryLooo@users.noreply.github.com>
2024-01-13 12:27:32 +0700

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,98 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import code from "./code.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import procreate from "./tech/procreate.png";
+import unity from "./tech/unity.png";
+import python from "./tech/python.png";
+import android from "./tech/android.png";
+import axure from "./tech/axure.jpg";
+
+import linkedin from "./tech/linkedin.svg";
+// import github from "./tech/github.svg";
+import instagram from "./tech/instagram.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import ybhk from "./company/ybhk.png"
+import sman from "./company/sman.png"
+import kmbd from "./company/kmbd.png"
+import binus from "./company/binus.png"
+import bgdc from "./company/bgdc.png"
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import panda from "./panda.png";
+import port3d from "./port3d.png";
+import enrichment from "./enrichment.jpg";
+import horizons from "./horizons.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  instagram,
+  linkedin,
+  ybhk,
+  sman,
+  kmbd,
+  binus,
+  bgdc,
+  procreate,
+  unity,
+  axure,
+  python,
+  android,
+  enrichment,
+  port3d,
+  panda,
+  code,
+  horizons
+};
---
+++ src/constants/constants.js
@@ -0,0 +1,339 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    instagram,
+    linkedin,
+    github,
+    ybhk,
+    sman,
+    kmbd,
+    binus,
+    bgdc,
+    procreate,
+    unity,
+    axure,
+    python,
+    android,
+    enrichment,
+    panda,
+    port3d,
+    code,
+    horizons
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "experience",
+      title: "Experience",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Game Programmer",
+      icon: web,
+    },
+    {
+      title: "Web Developer",
+      icon: mobile,
+    },
+    {
+      title: "Front End Developer",
+      icon: backend,
+    },
+    {
+      title: "Artist",
+      icon: creator,
+    },
+  ];
+  
+  const socials = [
+    {
+      id:1,
+      icon: linkedin,
+      url: "linkedin.com"
+    },
+    {
+      id:2,
+      icon: instagram,
+      url:"instagram.com"
+    },
+    {
+      id:3,
+      icon: github,
+      url:"github.com"
+    }
+  ]
+
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "procreate",
+      icon: procreate,
+    },
+    {
+      name: "unity",
+      icon: unity,
+    },
+    {
+      name: "android",
+      icon: android,
+    },
+    {
+      name: "python",
+      icon: python,
+    },
+    {
+      name: "axure",
+      icon: axure,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Head of Student Council at Damai Junior Highschool",
+      company_name: "YBHK",
+      icon: ybhk,
+      iconBg: "#383E56",
+      date: "2015 - 2016",
+      points: [
+        "Developed strong communication skills through regular interaction with students, teachers, and school administrators.",
+        "Successfully organized and coordinated school events, such as school festivals, competitions, and social gatherings.",
+        "Managed and guided a team of fellow students in organizing and executing various school events and activities.",
+        "Demonstrated effective leadership by serving as the Head of Student Council.",
+      ],
+    },
+    {
+      title: "Rohani Buddha Activist",
+      company_name: "SMAN 19",
+      icon: sman,
+      iconBg: "#E6DEDD",
+      date: "2018 - 2019",
+      points: [
+        "Organized and participated in events that fostered spiritual growth and connection among community members.",
+        "Organized educational workshops or seminars on Buddhist philosophy, ethics, and meditation techniques.",
+        "Managed and guided a team of fellow students in organizing and executing various Buddhist events and activities.",
+      ],
+    },
+    {
+      title: "Game Programmer and 2D Artist",
+      company_name: "Binus Game Developer Club",
+      icon: bgdc,
+      iconBg: "#FFFFFF",
+      date: "2021 - 2022",
+      points: [
+        "Actively participated in game jams, hackathons, or coding competitions, showcasing a commitment to continuous learning and creative problem-solving.",
+        "Led a team of game programmers in conceptualizing, designing, and developing interactive and engaging video game projects.",
+        "Oversaw the entire game development lifecycle, from ideation to implementation, ensuring projects were delivered on time and within scope.",
+        "Produced comprehensive technical documentation to aid in the understanding and maintenance of game code. Ensured effective communication of technical concepts within the team.",
+      ],
+    },
+    {
+      title: "Internal Relation Activist",
+      company_name: "Keluarga Mahasiswa Buddhis Dhammavaddhana",
+      icon: kmbd,
+      iconBg: "#E6DEDD",
+      date: "2021 - 2022",
+      points: [
+        "Successfully increased attendance and participation in internal gatherings through strategic planning and engaging promotional strategies..",
+        "Managed event budgets effectively, ensuring cost efficiency while delivering engaging and memorable gatherings.",
+        "Successfully organized and coordinated organization events, such as festivals, and social gatherings.",
+        "Conducted outreach initiatives to connect with local communities, religious organizations, and individuals to encourage participation in KMBD's Magha Puja as Staff of Public Relations",
+        "Developed and implemented comprehensive fundraising strategies to secure financial support for KMBD's Asadha Puja as Head of Funding Division",
+      ],
+    },
+    {
+      title: "Division Coordinator",
+      company_name: "Keluarga Mahasiswa Buddhis Dhammavaddhana",
+      icon: kmbd,
+      iconBg: "#E6DEDD",
+      date: "2022 - 2023",
+      points: [
+        "Coordinated Division 3 'Secretariat, Entrepreneurship, and Internal Relation' of Keluarga Mahasiswa Buddhis Dhammavaddhana at Kemanggisan",
+        "Led a team of activists to redesign existing merch into brand new high-quality products.",
+        "Orchestrated the planning and execution of various gathering events, ensuring seamless coordination among Internal Relation activists for support.",
+        "Collaborated with other divisions within Keluarga Mahasiswa Buddhis Dhammavaddhana to ensure integrated event planning and successful execution.",
+        "Advisor for Magha Puja and Buddhist Camp",
+        "Event Coordinator of Latihan Kepemimpinan Manajemen Buddhis Aktivis",
+        "Project Manager for Latihan Kepemimpinan Manajemen Buddhis Calon Pengurus",
+        "Head of Design and Documentation for Malam Apresiasi"
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "The Tragic Enrichment",
+      description:
+        "The Tragic Enrichment is a 3D FPS Horror Game that has you exploring a basement of rooms and corridors to find the keys to escape the basement.",
+      tags: [
+        {
+          name: "Unity",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "C++",
+          color: "green-text-gradient",
+        },
+        {
+          name: "VSC",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: enrichment,
+      source_code_link: "https://henryloo.itch.io/the-tragic-enrichment",
+    },
+    {
+      name: "Forgotten Red Panda",
+      description:
+        "Forgotten Red Panda is a 2D side-scrolling adventure game that has you navigating a lost and forgotten red panda out of its former bamboo forest home. ",
+      tags: [
+        {
+          name: "Unity",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "C++",
+          color: "green-text-gradient",
+        },
+        {
+          name: "VSC",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: panda,
+      source_code_link: "https://bgdc.itch.io/forgotten-red-panda",
+    },
+    {
+      name: "Horizons",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "Figma",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Behance",
+          color: "green-text-gradient",
+        },
+      ],
+      image: horizons,
+      source_code_link: "https://www.behance.net/gallery/164237103/Horizons",
+    },
+    {
+      name: "3js + React Portofolio Website",
+      description:
+        "A 3D Portofolio website made using the combination of multiple languages, mainly react and 3js.",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "3js",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: port3d,
+      source_code_link: "https://henryloo.vercel.app/",
+    }
+  ];
+  
+  export { socials, services, technologies, experiences, testimonials, projects };
